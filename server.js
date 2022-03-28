9922e27faed082599a4755f950d996d1ef083382
Chahak <chahak.shah.cs@gmail.com>
2022-03-28 17:33:47 +0530

    handled error

--- server.js
+++ server.js
@@ -23,29 +23,33 @@ mongoose
   })
   .then(() => {
     app.listen(PORT, async () => {
-      console.log(`Server running on port: ${PORT}`);
-      await Moralis.start({ serverUrl, appId });
+      try {
+        console.log(`Server running on port: ${PORT}`);
+        await Moralis.start({ serverUrl, appId });
 
-      // eth
-      const eth = cron.schedule("0 * * * *", async () => {
-        console.log("running");
-        await getLatestContract(
-          new Web3(`https://mainnet.infura.io/v3/${process.env.INFURA_KEY}`),
-          Moralis,
-          "eth"
-        );
-      });
-      eth.start();
+        // eth
+        const eth = cron.schedule("0 * * * *", async () => {
+          console.log("running");
+          await getLatestContract(
+            new Web3(`https://mainnet.infura.io/v3/${process.env.INFURA_KEY}`),
+            Moralis,
+            "eth"
+          );
+        });
+        eth.start();
 
-      // polygon
-      const polygon = cron.schedule("30 * * * *", async () => {
-        console.log("running");
-        await getLatestContract(
-          new Web3(process.env.POLYGON_SERVER_URL),
-          Moralis,
-          "matic"
-        );
-      });
-      polygon.start();
+        // polygon
+        const polygon = cron.schedule("30 * * * *", async () => {
+          console.log("running");
+          await getLatestContract(
+            new Web3(process.env.POLYGON_SERVER_URL),
+            Moralis,
+            "matic"
+          );
+        });
+        polygon.start();
+      } catch (error) {
+        console.log(error);
+      }
     });
   });
