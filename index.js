6d412a97efb953b5ddd23ec1e1a69b246240cdb8
chahakshahcs5 <76069775+chahakshahcs5@users.noreply.github.com>
2022-03-30 02:42:04 +0530

    Decreased loop

--- index.js
+++ index.js
@@ -24,8 +24,8 @@ const getLatestContract = async (web3, Moralis, chain) => {
     const data = await generalDb.find({});
     if (chain == "eth") {
       const toBLock =
-        latestBlock - data[0].LastEthBlock > 5000
-          ? data[0].LastEthBlock + 5000
+        latestBlock - data[0].LastEthBlock > 3000
+          ? data[0].LastEthBlock + 3000
           : latestBlock;
       console.log(toBLock);
       for (let i = data[0].LastEthBlock; i < toBLock; i++) {
@@ -37,8 +37,8 @@ const getLatestContract = async (web3, Moralis, chain) => {
       );
     } else {
       const toBLock =
-        latestBlock - data[0].LastPolygonBlock > 5000
-          ? data[0].LastPolygonBlock + 5000
+        latestBlock - data[0].LastPolygonBlock > 3000
+          ? data[0].LastPolygonBlock + 3000
           : latestBlock;
       for (let i = data[0].LastPolygonBlock; i < toBLock; i++) {
         await getContractByBlock(i, Moralis, chain);
